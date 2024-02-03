47d248964fbfcceb120128083eb4d29234348dbf
cesarLDsousa <cesarluiz.git@gmail.com>
2024-02-03 18:29:06 -0300

    add base controller to core

---
+++ App/Core/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Core;
+
+class BaseController 
+{
+    public function model($model)
+    {
+        require_once "../App/Models/".$model.".php";
+        return new $model;
+    }
+}
